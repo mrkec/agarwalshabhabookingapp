59e363f7420db6b548bf162376f845df86d95d55
eRozgaar <mrkec1994@gmail.com>
2024-02-24 11:28:19 +0530

    agarwalshabhabookingapp

---
+++ models/TempBookingModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const tempBookingSchema = new mongoose.Schema(
+  {
+    planId: {
+      type: Number,
+      required: true,
+    },
+    planName: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    userMobile: {
+      type: String,
+      required: true,
+    },
+    tempBookingDate: {
+      type: String,
+      required: true,
+    },
+    isConformed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const tempBooking = mongoose.model("TempBooking", tempBookingSchema);
+
+module.exports = tempBooking;
